af3347486753eca9fe13844389b35ae8a28149c6
RAELTO <elias-toro@outlook.com>
2021-12-01 23:29:02 -0500

    Actualizacion frontend

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const { swaggerServe, swaggerSetup } = require('./src/settings/swagger');
-
+const path = require('path');
 const { connectDB } = require('./src/settings/db.connection');
 const { Config } = require('./src/settings/env.config');
 const routes = require('./src/routes/routes');
+const ejs = require('ejs');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/views/index.html')
+})
+
 //db connection
 connectDB(Config.mongoUri);
-
-
 
 //midlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/doc', swaggerServe, swaggerSetup);
+app.use(express.static(__dirname + '/src/views'));
 
 
 
